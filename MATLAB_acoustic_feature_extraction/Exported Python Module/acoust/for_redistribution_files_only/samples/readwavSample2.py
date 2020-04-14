d0776fd2040a438ee5bb565780afa85b6a7b037a
dgrah50 <d.graham50@hotmail.co.uk>
2020-04-14 17:50:49 +0100

    initial commit - V1 of Matlab script to extract audio features. Features include melceps +1st/2nd derivs, pitch and energy

---
+++ MATLAB_acoustic_feature_extraction/Exported Python Module/acoust/for_redistribution_files_only/samples/readwavSample2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Sample script that uses the acoust module created using
+MATLAB Compiler SDK.
+
+Refer to the MATLAB Compiler SDK documentation for more information.
+"""
+from __future__ import print_function
+# import acoust
+import matlab
+import os
+
+cwd = os.getcwd()
+print(cwd)
+
+# my_acoust = acoust.initialize()
+
+# wavfileIn = "osr1.wav"
+# fvOut = my_acoust.readwav(wavfileIn)
+# print(fvOut, sep='\n')
+
+# my_acoust.terminate()
---
+++ MATLAB_acoustic_feature_extraction/readwav.m
@@ -0,0 +1,117 @@
+%{
+    ACOUSTIC FEATURE EXTRACTOR SCRIPT.
+    WAV file -> N-dimension feature vector
+    The WAV file is analysed in 100ms frames
+    For each 100ms frame a feature vector is generated containing the following information:
+    The min, max, median, mean and range of the pitch (based on 10ms subframes)
+    The min, max, median, mean and range of the energy (based on 10ms subframes)
+    Mel Cepstral Coefficients + 1st and 2nd derivatives
+%}
+
+
+function fv = acousticfeaturevec(wavfile)
+    %%%%%%%%%%%%%%%%%%% PRELIMINARY SECTION AND SETUP %%%%%%%%%%%%%%%%%%%
+    %Sample frequency
+    fs = 8000;
+
+    %We are analysing 100ms segments of the signal and assigning a prominence score to each segment
+    binlength = 0.1;
+
+    %Load wav
+    [waveform] = v_readwav(wavfile, 'p', -1, 0);
+
+    %%%%%%%%%%%%%%%%%%% VOICE ACTIVIY LEVEL FEATURE EXTRACTION %%%%%%%%%%%%%%%%%%%
+    % Calculate approximate voice activity level from p.56 activity level
+    %  [a, b, c, VAD] = v_activlev(waveform, 8000, 'n');
+    [vs] = v_vadsohn(waveform, fs, 'nb');
+    vad = vadframesplit(vs(:, 3));
+
+    %%%%%%%%%%%%%%%%%%% PITCH FEATURE EXTRACTION %%%%%%%%%%%%%%%%%%%
+    %pitch estimation algorithm - give analysis in 10ms frames
+    [pitch_est] = v_fxpefac(waveform, fs, 0.01);
+
+    % %calculates the min,max, median, mean and range of the pitch within a 100
+    % ms frames
+    [pitchstats] = pitchframesplit(pitch_est);
+
+    %%%%%%%%%%%%%%%%%%% MELCEPT FEATURE EXTRACTION %%%%%%%%%%%%%%%%%%%
+    melp = melcepstcalc(waveform,fs,binlength);
+
+    %%%%%%%%%%%%%%%%%%% CONSTRUCTION OF FEATURE VECTOR %%%%%%%%%%%%%%%%%%%
+     fv = constructfeaturevector(vad,pitchstats,melp);
+     
+     %%%%%%%%%%%%%%%%%%% HELPER FUNCTIONS %%%%%%%%%%%%%%%%%%%
+
+function [melp] = melcepstcalc(signal, fs,binlength)
+    melp = v_melcepst(signal, fs, 'E', 12, floor(3 * log(fs)), 1600);
+    %{
+    melp Inputs:
+     s      speech signal
+     fs  sample rate in Hz (default 11025)
+     w   mode string (see below)
+     nc  number of cepstral coefficients excluding 0'th coefficient [default 12]
+     p   number of filters in v_filterbank [default: floor(3*log(fs)) =  approx 2.1 per ocatave]
+     n   length of frame in samples [default power of 2 < (0.03*fs)]
+     inc frame increment [default n/2]
+     fl  low end of the lowest filter as a fraction of fs [default = 0]
+     fh  high end of highest filter as a fraction of fs [default = 0.5]
+    %}
+    
+    % Calculate first and second order derivatives of the melceps
+    % Concatenate zeros to the front to adjust for length
+    firstdiff = cat(1, zeros(1, 13), diff(melp));
+    seconddiff = cat(1, zeros(2, 13), diff(melp,2));
+    melp = horzcat(horzcat(melp, firstdiff), seconddiff);
+end
+
+function [pitchstats] = pitchframesplit(signal)
+    % Number of groups
+    nGroup = floor(length(signal)*0.1);
+    % Number of element per each group, should 
+    nElementPerGroup = 10;
+    % Create a group index
+    group = repelem((1:nGroup), nElementPerGroup);
+    % Transpose signal to use splitapply
+    signal = transpose(signal);
+    % Stats of each group
+    [lo, hi, med, avg, rng] = splitapply(@multiStat, signal(1:numel(group)), group);
+    pitchstats = vertcat(lo, hi, med, avg, rng);
+    pitchstats = transpose(pitchstats);
+end
+
+function [avg] = vadframesplit(signal)
+    % Number of groups
+    nGroup = floor(length(signal) * 0.1);
+    % Number of element per each group, should
+    nElementPerGroup = 10;
+    % Create a group index
+    group = repelem((1:nGroup), nElementPerGroup);
+    % Transpose signal to use splitapply
+    signal = transpose(signal);
+    % Stats of each group
+    [avg] = splitapply(@mean, signal(1:numel(group)), group);
+    avg = transpose(avg);
+
+end
+
+function [lo, hi, med, avg, rng] = multiStat(x)
+    lo = min(x);
+    avg = mean(x);
+    med = median(x);
+    rng = range(x);
+    hi = max(x);
+end
+
+function [fv] = constructfeaturevector(vad,pitchstats,melp) 
+    %Truncate all feature vectors before concatenation
+    minlen = min([length(vad),length(pitchstats),length(melp)]);
+    vad = vad(1:minlen, :);
+    pitchstats = pitchstats(1:minlen, :);
+    melp =  melp(1:minlen, :);
+    %Concatenate feature vectors
+    fv = horzcat(horzcat(vad,pitchstats),melp);
+end
+
+end
+
+
